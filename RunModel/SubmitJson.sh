55898291be3f47a8313c3871eec6354c3682ab9c
bw4sz <benweinstein2010@gmail.com>
2017-02-21 08:17:41 -0800

    correctly run prediction

--- RunModel/SubmitJson.sh
+++ RunModel/SubmitJson.sh
@@ -6,11 +6,6 @@
 gcloud compute instances create gci --image-family gci-stable --image-project google-containers --scopes 773889352370-compute@developer.gserviceaccount.com="https://www.googleapis.com/auth/cloud-platform" --boot-disk-size "40"
 gcloud compute ssh benweinstein2010@gci 
 
-#get cloudml docker environment, run as privileged to allow mount
-#docker pull gcr.io/cloud-datalab/datalab:local
-#docker run --privileged -it --rm  -p "127.0.0.1:8080:8080" \
-  --entrypoint=/bin/bash \
-  gcr.io/cloud-datalab/datalab:local
 
 #Custom docker env
 docker pull gcr.io/api-project-773889352370/cloudml:latest
@@ -53,17 +48,21 @@ pip install imutils
 #extract letters
 python MeerkatReader/RunModel/main.py -indir mnt/gcs-bucket/Cameras/$MONTH/ -outdir mnt/gcs-bucket/Cameras/$MONTH/letters/ -limit=5 
 
+gsutil ls gs://api-project-773889352370-ml/Cameras/$MONTH/*.jpg > jpgs.txt
+
+python MeerkatReader/RunModel/images_to_json.py -o mnt/gcs-bucket/Cameras/$MONTH/request.json $(cat jpgs.txt)
+
 #submit job
 JOB_NAME=predict_Meerkat_$(date +%Y%m%d_%H%M%S)
 gcloud beta ml jobs submit prediction ${JOB_NAME} \
     --model=${MODEL_NAME} \
     --data-format=TEXT \
-    --input-paths=gs://api-project-773889352370-ml/Cameras/$MONTH/letters/ \
+    --input-paths=gs://api-project-773889352370-ml/Cameras/$MONTH/request.json \
     --output-path=gs://api-project-773889352370-ml/Cameras/$MONTH/prediction/ \
     --region=us-central1
     
 #describe job
- gcloud beta ml jobs describe ${JOB_NAME}
+gcloud beta ml jobs describe ${JOB_NAME}
  
 #exit ssh
 exit
